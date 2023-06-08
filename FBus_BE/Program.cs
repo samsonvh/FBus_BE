bf07286fc0a33e62a991f18b284fb0c2a11335ca
phamquangthang119 <phamquangthang119@gmail.com>
2023-06-08 11:28:09 +0700

    Update all repository and service in Program.cs

--- FBus_BE/Program.cs
+++ FBus_BE/Program.cs
@@ -1,3 +1,7 @@
+using FBus_BE.Data;
+using FBus_BE.Repository;
+using FBus_BE.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<FbusMainContext>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//account
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+//driver
+builder.Services.AddScoped<IDriverRepository, DriverRepository>();
+builder.Services.AddScoped<IDriverService, DriverService>();
+//route
+builder.Services.AddScoped<IRouteRepository, RouteRepository>();
+builder.Services.AddScoped<IRouteService, RouteService>();
+//station
+builder.Services.AddScoped<IStationRepository, StationRepository>();
+builder.Services.AddScoped<IStationService, StationService>();
 
 var app = builder.Build();
 
